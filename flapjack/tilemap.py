ee2dfc8eb10cceb8ca5553690ba22977cc55be42
Jay Newey <jayluxray@gmail.com>
2020-06-20 15:56:19 +0100

    Add support for multiple layers

--- flapjack/tilemap.py
+++ flapjack/tilemap.py
@@ -5,6 +5,7 @@ class TileMap:
     def __init__(self, map_dict, tile_dict, tileset, colorkey=None):
         self.tile_dict = tile_dict
         self.tileset = tileset
+
         self.map_dict = map_dict
         self.chunks = map_dict["chunks"]
         self.chunk_width, self.chunk_height = map_dict["chunk_size"]
@@ -12,20 +13,31 @@ class TileMap:
 
         self.tile_width, self.tile_height = map_dict["tile_size"]
 
-        self.chunk_surfaces = {chunk: self._render_chunk(chunk) for chunk in self.chunks}
+        self.chunk_surfaces = {chunk: self._render_chunk(chunk) for chunk in self.chunks.keys()}
 
     def _render_chunk(self, chunk):
-        """Renders a whole chunk onto a surface.
+        """Returns a list of rendered layer surfaces for this chunk.
 
         :param chunk: The chunk key
         :type chunk: str
+        :return: The rendered layers of the chunk
+        :rtype: list
+        """
+        return [self._render_layer(layer) for layer in self.chunks[chunk]["layers"]]
+
+    def _render_layer(self, layer):
+        """Renders a chunk layer onto a surface.
+
+        :param layer: The chunk layer you want to render
+        :type layer: list
         :return: The rendered chunk surface
         :rtype: pygame.Surface
         """
         surface = pygame.Surface((self.tile_width * self.chunk_width, self.tile_height * self.chunk_height))
+        print(layer)
         for y in range(self.chunk_height):
             for x in range(self.chunk_width):
-                tile_id = self._get_tile_id_at(chunk, x, y)
+                tile_id = str(layer[y][x])
                 if tile_id in self.tile_dict.keys():
                     tile_texture = self.get_tile_texture(tile_id)
                     surface.blit(tile_texture, (x * self.tile_width, y * self.tile_height))
@@ -33,26 +45,12 @@ class TileMap:
             surface.set_colorkey(self.colorkey)
         return surface
 
-    def _get_tile_id_at(self, chunk, x, y):
-        """Gets the tile id at the x,y coordinate in the chunk given.
-
-        :param chunk: The chunk key
-        :type chunk: str
-        :param x: The x coordinate of the tile you want
-        :type x: int
-        :param y: The y coordinate of the tile you want
-        :type y: int
-        :return: The tile id at the coordinate
-        :rtype: str
-        """
-        return str(self.chunks[chunk][y][x])
-
-    def _get_tile_data_at(self, chunk, x, y):
+    def _get_tile_data_at(self, layer, x, y):
         """Gets the tile at the x,y coordinate in the chunk given.
         An empty dict if the tile isn't a recognised id e.g no tile at that location (usually denoted by -1).
 
-        :param chunk: The chunk key
-        :type chunk: str
+        :param layer: The list of tile ids
+        :type layer: list
         :param x: The x coordinate of the tile you want
         :type x: int
         :param y: The y coordinate of the tile you want
@@ -60,7 +58,7 @@ class TileMap:
         :return: The tile at the coordinate
         :rtype: dict
         """
-        tile_id = self._get_tile_id_at(chunk, x, y)
+        tile_id = str(layer[y][x])
         return self.tile_dict[tile_id] if tile_id in self.tile_dict.keys() else {}
 
     def get_tile_texture(self, tile_id):
@@ -75,12 +73,14 @@ class TileMap:
         region = pygame.Rect(column * self.tile_width, row * self.tile_height, self.tile_width, self.tile_height)
         return self.tileset.subsurface(region)
 
-    def get_overlapping_tiles(self, rect, properties=()):
+    def get_overlapping_tiles(self, rect, layer_index=0, properties=()):
         """Get all the tiles that overlap the given rect that have the given properties set to true.
         Gets any overlapping tiles if no properties are specified.
 
         :param rect: The rectangle you want to check overlapping for
         :type rect: pygame.Rect
+        :param layer_index: The index of the layer you want to get the overlapping tiles on. Bottom layer by default.
+        :type layer_index: int
         :param properties: A list of properties that the tiles must declare to be true
         :type properties: tuple
         :return: A list of overlapping tile rectangles
@@ -95,7 +95,8 @@ class TileMap:
             relative_x = (x - real_chunk_x) // self.tile_width
             relative_y = (y - real_chunk_y) // self.tile_height
             if chunk in self.chunks.keys():
-                tile = self._get_tile_data_at(chunk, relative_x, relative_y)
+                layer = self.chunks[chunk]["layers"][layer_index]
+                tile = self._get_tile_data_at(layer, relative_x, relative_y)
                 if all(tile[p] if p in tile.keys() else False for p in properties):
                     overlapping_tiles.append(pygame.Rect(real_chunk_x + relative_x * self.tile_width,
                                                          real_chunk_y + relative_y * self.tile_height,
